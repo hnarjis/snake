472e4bfcade43aaa430f9e44c46d95d85176479a
Narjis Hachilif <hnarjis@gmail.com>
2014-11-21 23:26:35 +0100

    Start building the snake

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
-var SNAKE = {};
-
-var Snake = (function () {
-  var canvas;
+var Game = function () {
   var ctx;
   var xPosition = 0;
-  var yPosition = 0;
-  var frameLength = 500;
+  var yPosition = 1;
+  var time = 100;
 
   function init() {
-	canvas = document.getElementById('canvas');
-	ctx = canvas.getContext('2d');
+	  ctx = document.getElementById('canvas').getContext('2d');
     start();
   }
 
   function start() {
     xPosition += 2;
-    yPosition += 4;
-    ctx.clearRect(0, 0, 100, 100);
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(xPosition, yPosition, 30, 50);
-    setTimeout(start, frameLength);
+    //yPosition += 4;
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.fillStyle = 'green';
+    ctx.fillRect(xPosition, yPosition, 20, 10);
+    setTimeout(start, time);
   }
 
   return {
     init: init
   };
-})();
-
+}();
 
 window.addEventListener('load', function() {
-  Snake.init();
+  Game.init();
 });
